54325a4a8ebe2f4e7be36eedc577d2d3734e84d6
thealamenthed <dalila.boualemfr@gmail.com>
2022-05-31 00:01:33 +0200

    update js and json

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
+
 // use micro framework express
 const express = require('express');
 
-// use axios
-const axios = require('axios');
-
-//use cors
-const cors = require('cors');
+// use a data file
+const data = require('./data/data.json');
 
 // use port 8000 to run server on localhost
 const port = 8000;
@@ -19,14 +17,25 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-// configure cors
-app.use(cors('*'));
-
 // default entry point '/' of the server => go to http://localhost:8000 after executing npm start
-app.get('/api/artists/:name', (req, res) => {
+app.get('/', (req, res) => {
     res.json({ message : 'Welcome on Express/Node Server'}).status(200);
 });
 
-app.get('/api/genres/:name', (req, res) => {
-    res.json({ message : 'Welcome on Express/Node Server'}).status(200);
+// log server start (check your terminal to see the message)
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+app.get('/api/author/:name', (req, res) => {
+
+    const item = data.find(data => data.name === req.params.name);
+    res.json(item).status(200);
+});
+
+app.get('/api/category/:name', (req, res) => {
+    
+    const item = data.filter(data => data.category === req.params.name);
+    /* console.log(req.params.name); */
+    res.json(item).status(200);
 });
